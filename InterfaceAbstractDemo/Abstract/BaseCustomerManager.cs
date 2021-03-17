55365c1793637a9f4a6a54fc9438b28024fd23d9
Gökalp Avcu <avcu219@gmail.com>
2021-03-17 21:48:22 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Abstract/BaseCustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public abstract class BaseCustomerManager : ICustomerCheckService
+    {
+        public virtual void Save(Customer customer)
+        {
+            Console.WriteLine("Saved to db : " +customer.FirstName);
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(1999,2,27), FirstName = "Gökalp" , LastName = "Avcu", NationalityId = "20838784512"});
+        }
+
+    }
+}
